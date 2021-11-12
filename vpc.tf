cc8b388d950fd83c2eb3c2c89313e888d6f2e4bb
Johan K Mathew <johanmathew@Johans-MacBook-Pro.local>
2021-11-12 17:47:23 +0530

    [Johan/Abin] Add internet_gateway and route_table, add entry to routetable and attach to vpc

--- vpc.tf
+++ vpc.tf
@@ -20,11 +20,33 @@ resource "aws_subnet" "welcome-demo-subnet-public-1" {
   }
 }
 
-resource "aws_network_interface" "welcome-demo-ni" {
-  subnet_id   = aws_subnet.welcome-demo-subnet-public-1.id
-  private_ips = ["10.0.1.100"]
-  security_groups = [aws_security_group.welcome-demo-sg.id]
+resource "aws_internet_gateway" "welcome-demo-ig" {
+  vpc_id = aws_vpc.welcome-demo-vpc.id
+
   tags = {
-    Name = "primary_network_interface"
+    Name = "welcome-demo-prod-abin-johan"
   }
+}
+
+# resource "aws_network_interface" "welcome-demo-ni" {
+#   subnet_id   = aws_subnet.welcome-demo-subnet-public-1.id
+#   private_ips = ["10.0.1.100"]
+#   security_groups = [aws_security_group.welcome-demo-sg.id]
+#   tags = {
+#     Name = "primary_network_interface"
+#   }
+# }
+
+resource "aws_route_table" "welcome-demo-rt" {
+  vpc_id = aws_vpc.welcome-demo-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.welcome-demo-ig.id
+  }
+
+}
+resource "aws_route_table_association" "rta_subnet_public" {
+  subnet_id      = aws_subnet.welcome-demo-subnet-public-1.id
+  route_table_id = aws_route_table.welcome-demo-rt.id
 }
